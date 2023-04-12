0068c118938a10f1ac5257347d668662fbd25417
bereketli <blingerew2018@gmail.com>
2023-04-12 12:07:48 +0300

    fist commit

---
+++ testimonial-grid-tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        "25px":"25px",
+      }
+    },
+  },
+  plugins: [],
+}
+
